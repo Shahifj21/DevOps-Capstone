3096ad47e5ecb859966f9fb81e602d01e9573086
Shahif A <ashahifw@gmail.com>
2026-08-09 05:51:41 +0000

    capstone project

--- infra/terraform/backend.tf
+++ infra/terraform/backend.tf
@@ -2,7 +2,7 @@ terraform {
   required_version = ">= 1.5.0"
 
   backend "s3" {
-    bucket         = "amzn-devops-accelerator-tf-state-shahif "
+    bucket         = "amzn-devops-accelerator-tf-state-shahif"
     key            = "terraform.tfstate"
     region         = "us-east-1"
     encrypt        = true   
